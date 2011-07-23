2055dd167b13ad85bbc19b327558741d430e0e14
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2011-07-23 17:06:37 +0200

    added some error handling, flexible in- and output filenames

--- src/main/scala/UserInterface.scala
+++ src/main/scala/UserInterface.scala
@@ -22,24 +22,37 @@ object UserInterface extends App {
     println("3) exit")
     println("4) debug infos")
 
-    readInt() match {
+    (try { readInt() } catch { case ex => -1 }) match {
       case 1 =>
-        println("please enter the filename")
-        val filename = readLine()
 
         val trainer = new Trainer()
-        trainer.learnFile(filename, 5, 12)
-        trainer.save("test.data")
+        print("input file: ")
+        trainer.learnFile(readLine(), 5, 12)
+
+        print("output file [test.data]: ")
+        val filename = readLine()
+        trainer.save( if (filename == "") "test.data" else filename )
       case 2 =>
         val generator = new Generator()
-        generator.load("test.data")
-        for (i <- 1 to 20) println(generator.generateWord(5, 12))
+        print("pattern file [test.data]: ")
+        val filename = readLine()
+        generator.load( if (filename == "") "test.data" else filename )
+        print("words to generate [20]: ")
+        val numWords = try {
+           readInt()
+        } catch {
+          case ex => 20
+        }
+        for (i <- 1 to numWords) println(generator.generateWord(5, 12))
       case 3 =>
         sys.exit()
       case 4 =>
         println()
         val generator = new Generator()
-        generator.load("test.data")
+        print("pattern file [test.data]: ")
+        val filename = readLine()
+        generator.load( if (filename == "") "test.data" else filename )
+
         for( (prevs, probs) <- generator.probs.toList.sortWith( (a, b) => (a._1._1*256 + a._1._2) < (b._1._1*256 + b._1._2))) {
           print(prevs)
           println(":")
